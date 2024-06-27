fb1d8bc1d02cc9d4314f231434aba60f513102dc
denmd <devendhumd1002@gmail.com>
2024-06-27 21:09:34 +0530

    sort_array_by_parity

---
+++ DSA/ARRAYS/sort_array_by_parity/sort_array_by parity.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def sortArrayByParity(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        j=0
+        for i in range(len(nums)):
+            if nums[i]%2==0:
+                nums[i],nums[j]=nums[j],nums[i]
+                j+=1
+        return nums
+
+        
